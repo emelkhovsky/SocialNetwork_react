629c4474368735d2e56b99062fb5f3173975f490
emelkhovsky <emelkhovsky4@mail.ru>
2020-07-22 00:54:29 +0300

    add start files

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './Header.module.css'
+
+const Header = () => {
+    return (
+        <header className={s.header}>
+            <img src="https://i.pinimg.com/600x315/13/e3/43/13e34340ef2625f926ed799e68b1a7e2.jpg" alt="" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import s from './Profile.module.css'
+
+const Profile = () => {
+    return (
+        <div className={s.profile}>
+            <div>
+                <img src="https://mskmebel.net/wp-content/uploads/2017/06/1361641418_brooklyn-bridge-in-night-1.jpg" alt="" />
+            </div>
+            <div>
+                <div>
+                    <img src="https://yt3.ggpht.com/a/AATXAJyZcXlLOhBLXh1c6UvHSz62WGhkRyju9z7N9MekbA=s900-c-k-c0xffffffff-no-rj-mo" alt="" />
+                </div>
+                <div>
+
+                </div>
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
